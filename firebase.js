208e29cd97a46cde97b8358705121cb1c310e195
miayush456 <miayush456@gmail.com>
2026-06-19 11:58:38 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR DOMAIN",
+  projectId: "YOUR PROJECT ID",
+};
